56b20955976c0d0e4991d8c5d3a67123c6864102
Brandon Reninger <brandonreninger@yahoo.com>
2020-06-01 23:43:47 -0600

    server linked

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-scpn1sxb.auth0.com";
+export const audience = "inspire.com";
+export const clientId = "Lo2QC7bUMzYxD4x1nYQ5XqiOAVI60flU";
